d8a40e8e8fbbb028555765372701f41e876c6e39
FarzamMohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2020-10-29 21:10:59 -0400

    Revert "Update. Added Buttons & Routes"
    This reverts commit a95f47eb835c026cd8b4a3a46681b7b7bf67af22.

--- server/routes/books.js
+++ server/routes/books.js
@@ -26,97 +26,44 @@ router.get('/', (req, res, next) => {
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-  res.render('books/details', {
-    title: 'Home',
-    books: book
-   });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
- 
-  let newBook = book({
-    "Title": req.body.title,
-    "Description": req.body.description,
-    "Price": req.body.price,
-    "Author": req.body.author,
-    "Genre": req.body.genre
-  });
 
-    book.create(newBook, (err, book) =>{
-      if(err)
-      {
-      console.log(err);
-      res.end(err);
-      }
-      else 
-      {
-      res.redirect('/books');
-      }
-    });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
 
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-  let id = req.params.id;
-
-  
-    book.findById(id, (err, bookToEdit) =>{
-        if(err)
-        {
-            console.log(err);
-            res.end(err); 
-        }
-        else
-        {
-            res.render('books/details', {title: 'Edit Contact', books: bookToEdit});
-        }
-    });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
-  let id = req.params.id;
 
-  let editedBook = book({
-    "_id": id,
-    "Title": req.body.title,
-    "Description": req.body.description,
-    "Price": req.body.price,
-    "Author": req.body.author,
-    "Genre": req.body.genre
-  });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
 
-  book.updateOne({_id: id}, editedBook, (err) =>{
-    if(err)
-    {
-      console.log(err);
-      res.end(err);
-    }
-    else
-      {
-        res.redirect('/books');
-      }
-   });
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-let id = req.params.id;
 
-  book.remove({_id: id}, (err) =>{
-    if(err)
-    {
-      console.log(err);
-      res.end(err);
-    }
-    else 
-    {
-    res.redirect('/books')
-    }
-  });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
 });
 
 
